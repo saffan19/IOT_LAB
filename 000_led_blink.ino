38d490c881a3f5e4989d384d9b9b1e914b6f2738
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-10-08 15:18:42 +0530

    Add files via upload

---
+++ 000_led_blink.ino
@@ -0,0 +1,15 @@
+int pin=13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pin,HIGH);
+  delay(500);
+  digitalWrite(pin,LOW);
+  delay(100);
+
+}
---
+++ 001_TRAFFIC_LIGHT.ino
@@ -0,0 +1,27 @@
+int red=13;
+int yellow=10;
+int green=8;
+int t=1000;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(red,HIGH);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,LOW);
+  delay(t);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,HIGH);
+  digitalWrite(green,LOW);
+  delay(t);
+  digitalWrite(red,LOW);
+  digitalWrite(yellow,LOW);
+  digitalWrite(green,HIGH);
+  delay(t);
+}
