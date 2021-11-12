e6cd5726af644dbef1bde9f2f12e010bf00aee9b
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-11-12 16:15:45 +0530

    Add files via upload

---
+++ 010_servo.ino
@@ -0,0 +1,43 @@
+#include<Servo.h>
+int dpin=5;
+int apin0=A0;
+int apinval;
+
+Servo myser;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(dpin,INPUT);
+Serial.begin(9600);
+myser.attach(9);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print("Do value:");
+Serial.println(digitalRead(dpin));
+Serial.print("A0 value:");
+apinval=analogRead(apin0);
+Serial.println(apinval);
+delay(1000);
+if(apinval>1000)
+{
+  
+  for(int pos=0;pos<=180;pos++)
+  {
+    myser.write(pos);
+    delay(10);
+  }
+  for(int pos=180;pos>=0;pos--)
+  {
+    myser.write(pos);
+    delay(10);
+  }
+  
+}
+else
+{
+  myser.write(0);
+}
+
+}
