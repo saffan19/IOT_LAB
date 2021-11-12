7bd0c0df09aaffe4d9055698488bef5e6421b1d6
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-11-12 15:22:19 +0530

    Add files via upload

---
+++ 006_temperature.ino
@@ -0,0 +1,18 @@
+int outputpin=0;
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int rawvolt=analogRead(outputpin);
+  float millivolt=(rawvolt/1024.0)*5000;
+  float celsius=millivolt/10;
+  Serial.print(celsius);
+  Serial.println(" C");
+  Serial.print((celsius*9)/5+32);
+  Serial.println(" F");
+  delay(1000);
+  
+
+}
