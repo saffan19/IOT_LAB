ec88e85fca122560b4cd53414b3bb778ac57996b
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-11-12 14:11:06 +0530

    Add files via upload

---
+++ 003_button.ino
@@ -0,0 +1,22 @@
+int button = 2;
+int led = 13;
+int buttonState=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(button,INPUT);
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(button);
+  if(buttonState == HIGH)
+  {
+    digitalWrite(led,HIGH);
+  }
+  else
+  {
+    digitalWrite(led,LOW);
+  }
+}
