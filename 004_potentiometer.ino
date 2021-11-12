dbbb563d2eb1efb9b90e0c83166c9e4df6860e31
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-11-12 15:09:47 +0530

    Add files via upload

---
+++ 004_potentiometer.ino
@@ -0,0 +1,17 @@
+int sensorValue=0;
+int outputValue=0;
+int analogOutPin=9;
+int analogPin=A0;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue=analogRead(analogPin);
+  outputValue=map(sensorValue,0,1023,0,255);
+  analogWrite(analogOutPin,outputValue);
+  Serial.println(sensorValue);
+  Serial.println(outputValue);
+  delay(1000);
+}
---
+++ 005_ldr.ino
@@ -0,0 +1,23 @@
+int ldrvalue=0;
+int light_sensitivity=40;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ldrvalue=analogRead(A0);
+delay(10);
+Serial.println(ldrvalue);
+if(ldrvalue<light_sensitivity)
+{
+  digitalWrite(9,HIGH);
+  
+}
+else
+{
+  digitalWrite(9,LOW);
+}
+}
