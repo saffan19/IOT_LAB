59a557f2f25bb0b290c48fbc5ad268a481d33434
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-11-12 15:54:37 +0530

    Add files via upload

---
+++ 007_ultrasonic.ino
@@ -0,0 +1,39 @@
+const int trig=8;
+const int echo=7;
+int duration=0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pinMode(trig,OUTPUT);
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  pinMode(echo,INPUT);
+  duration=pulseIn(echo,HIGH);
+  int inches=microsecondsToInches(duration);
+  int cm=microsecondsToCentimeters(duration);
+  Serial.print(inches);
+  Serial.println(" in");
+  Serial.print(cm);
+  Serial.println(" cm");
+  delay(100);
+
+}
+long microsecondsToInches(long ms)
+{
+  return ms/74/2;
+}
+long microsecondsToCentimeters(long ms)
+{
+  return ms/29/2;
+}
+
+
+
---
+++ 009_moisture.ino
@@ -0,0 +1,20 @@
+int dpin=5;
+int apin0=1;
+int apinval;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(dpin,INPUT);
+Serial.begin(9600);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.print("Do value:");
+Serial.println(digitalRead(dpin));
+Serial.print("A0 value:");
+apinval=analogRead(apin0);
+Serial.println(apinval);
+delay(5000);
+
+}
