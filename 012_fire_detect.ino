d5e306097330df401433e874c1b169a07e0c1469
Saffan Md <67745780+saffan19@users.noreply.github.com>
2021-12-03 15:11:58 +0530

    Add files via upload

---
+++ 012_fire_detect.ino
@@ -0,0 +1,26 @@
+#include<SoftwareSerial.h>
+//#include<NewPing.h>
+int sensorPin=A0;
+int sensorVal=0;
+int led=13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorVal=analogRead(sensorPin);
+  Serial.println(sensorVal);
+  if(sensorVal<500)
+  {
+    Serial.println("Fire Detected");
+    digitalWrite(led,HIGH);
+    delay(1000);
+  }
+  digitalWrite(led,LOW);
+  delay(sensorVal);
+
+}
